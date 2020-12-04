d84443d19800ab8f11cf032e880a9d0f26e78c3c
AlexiaTdev <alexia.teissier1@epsi.fr>
2020-12-04 15:00:38 +0100

    css inclu

--- pendu.js
+++ pendu.js
@@ -1,18 +1,18 @@
 var dictionnaire = ["soleil", "ocean", "peluche", "noel"]
 
 function displayBlocks(){
-    $('div#startgame').css("display","none");
-    $('div#ingame').css("display","block");
+    $('#startgame').css('display','none');
+    $('#ingame').css('display','block');
 }
 
 function selectMot(){
     var mot ="";
     mot=  dictionnaire[Math.floor(Math.random() * Math.floor(dictionnaire.length))];
     console.log(mot);
-    displayBlocks()
+    displayBlocks();
     return mot;
 }
 
-
 $('button#startbutton').on('click', $('div#motatrouver').text(selectMot()));
+$('#startbutton').on('click', console.log("rassuretoi je fonctionne"));
 
