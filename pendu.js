d4968ec48d93eb93935564c0eb5b0d6f759260bf
AlexiaTdev <alexia.teissier1@epsi.fr>
2020-12-08 17:09:34 +0100

    maj scoreboard + fin partie

--- pendu.js
+++ pendu.js
@@ -1,108 +1,148 @@
-var dictionnaire = ["SOLEIL", "OCEAN", "PELUCHE", "NOEL", "BATTRE"];
+var dictionnaire = ["ALTITUDE","PARCOURS","LONGUEUR","ORIGINAL","VICTOIRE"];
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lettresMot = [];
 var lettreMotAffiche = [];
 var nbrError=0;
+var mot ="";
+
+//variables environnement de jeu
+var nbrErreurMaximum=4;
 
 function initialization(){
-    $('#startgame').css('display','none');
     $('#ingame').css('display','block');
     $('div#motatrouver').text(selectMot);
 }
 
 function selectMot(){
-    var mot ="";
     mot=  dictionnaire[Math.floor(Math.random() * Math.floor(dictionnaire.length))];
-    
     var motHide="";
         //// creation du tableau mot entier
     for (i = 0; i< mot.length; i++) {
         lettresMot[i] = mot.charAt(i);
         lettreMotAffiche[i] = mot.charAt(i);
     }
-
         //// creation du tableau du mot a afficher avec _
-    //lettreMotAffiche= lettresMot;
-    //console.log(lettreMotAffiche[1])
     for (i = 1; i< lettreMotAffiche.length -1; i++) {
         lettreMotAffiche[i] = "_";
     }
-    
     motHide = hiddenMot();
     return motHide;
 }
 
 function hiddenMot(){
     var motHide="";
-        ////former le motHide a partir du tableau du mot a afficher
     for (i = 0; i< lettreMotAffiche.length; i++) {
         motHide = motHide + lettreMotAffiche[i] + " ";
     }
     return motHide;
 }
 
-
 function replay(){
-    $('#startgame').css('display','block'),
-    $('#ingame').css('display','non')
+    $('div#erreurs').text("");
+    $('div#resultat').text("");
+    for (const i in alphabet) {
+        $('button#lettre'+alphabet[i]).prop( "disabled", false);
+    }
+    nbrError=0;
+    initialization();
 }
 
 function playLetter(letter){
-    //console.log('je suis une lettre ' + letter);
-    //console.log(lettresMot[0] + " " + lettresMot[1])
-    //console.log(lettreMotAffiche[0] + " " + lettreMotAffiche[1])
     var lettretrouvee = false;
     $('button#lettre'+letter).prop( "disabled", true );
-    for (i = 1; i< lettresMot.length; i++) {
-        
+    for (i = 1; i< lettresMot.length-1; i++) {
         if (lettresMot[i]==letter){
-            //console.log("je suis la lettre identique "+letter)
             lettreMotAffiche[i]=letter;
             lettretrouvee =true;
         }
     }
     if (!lettretrouvee){
         error();
+    }else {
+        verifierSiMotTrouve()
     }
+    
     $('div#motatrouver').text(hiddenMot);
+}
+
+function verifierSiMotTrouve(){
+    motidentique = true;
+    for (i = 1; i< lettreMotAffiche.length-1; i++) {
+        if (lettreMotAffiche[i] != lettresMot[i]) {
+            motidentique = false;
+        }
+    }
+    if (motidentique){
+        afficherResultat(true);
+    }
 }
 
 function error(){
     nbrError=nbrError+1;
-    $('div#erreurs').prepend("Erreur vous etes mauvais");
-    if (nbrError==2){
-        $('div#erreurs').append("PERDU");
+    if (nbrError < nbrErreurMaximum){
+    $('div#erreurs').prepend("Erreur "+nbrError+"\/"+nbrErreurMaximum+"</br>");
+    } else {
+        afficherResultat(false);
     }
 }
 
+function afficherResultat(resultat){
+    for (const i in alphabet) {
+        $('button#lettre'+alphabet[i]).prop( "disabled", true );
+    }
+    if (resultat==true){
+        $('div#resultat').text("BRAVO Le mot a trouver etait bien "+ mot);
+        afficherScoreBoard();
+    }else {
+        $('div#resultat').text("PERDU Le mot à trouver était "+ mot);
+    }
+}
+
+function afficherScoreBoard(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "score.json", false);
+    request.send(null)
+    var my_JSON_object = JSON.parse(request.responseText);
+
+
+    var p = document.getElementById('scoreBoard');
+    var premiereligne = document.createElement('table');
+    var tr = document.createElement('tr');
+    var th = document.createElement('th');
+
+    th.textContent = "NAME";
+    tr.append(th);
+    var th2 = document.createElement('th');
+    th2.textContent = "SCORE";
+    tr.append(th2);
+    //on ajoute la ligne header du tableau
+    premiereligne.append(tr);
+
+
+    //on ajoute une ligne par element du json
+    for (key in my_JSON_object){
+        var trInt = document.createElement('tr');
+
+        var info = document.createElement('td');
+        info.textContent = key;
+        trInt.append(info);
+
+        var info2 = document.createElement('td');
+        info2.textContent = my_JSON_object[key];
+        console.log(my_JSON_object[key]);
+        trInt.append(info2);
+
+        premiereligne.append(trInt);
+    }
+
+    p.append(premiereligne);
+}
 
 
 $(document).ready(function() {
-    $('button#startbutton').on('click', initialization);
+    initialization();
     $('button#replay').on('click', replay);
-    $('button#lettreA').on('click', function() {playLetter("A");} );
-    $('button#lettreB').on('click', function() {playLetter('B');} );
-    $('button#lettreC').on('click', function() {playLetter('C');} );
-    $('button#lettreD').on('click', function() {playLetter('D');} );
-    $('button#lettreE').on('click', function() {playLetter('E');} );
-    $('button#lettreF').on('click', function() {playLetter('F');} );
-    $('button#lettreG').on('click', function() {playLetter('G');} );
-    $('button#lettreH').on('click', function() {playLetter('H');} );
-    $('button#lettreI').on('click', function() {playLetter('I');} );
-    $('button#lettreJ').on('click', function() {playLetter('J');} );
-    $('button#lettreK').on('click', function() {playLetter('K');} );
-    $('button#lettreL').on('click', function() {playLetter('L');} );
-    $('button#lettreM').on('click', function() {playLetter('M');} );
-    $('button#lettreN').on('click', function() {playLetter('N');} );
-    $('button#lettreO').on('click', function() {playLetter("O");} );
-    $('button#lettreP').on('click', function() {playLetter('P');} );
-    $('button#lettreQ').on('click', function() {playLetter('Q');} );
-    $('button#lettreR').on('click', function() {playLetter('R');} );
-    $('button#lettreS').on('click', function() {playLetter('S');} );
-    $('button#lettreT').on('click', function() {playLetter('T');} );
-    $('button#lettreU').on('click', function() {playLetter('U');} );
-    $('button#lettreV').on('click', function() {playLetter('V');} );
-    $('button#lettreW').on('click', function() {playLetter('W');} );
-    $('button#lettreX').on('click', function() {playLetter('X');} );
-    $('button#lettreY').on('click', function() {playLetter('Y');} );
-    $('button#lettrez').on('click', function() {playLetter('Z');} );
+    for (const i in alphabet) {
+        $('button#lettre'+alphabet[i]).on('click', function() {playLetter(alphabet[i]);} );
+    }   
 })
